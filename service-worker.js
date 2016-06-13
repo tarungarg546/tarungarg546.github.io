2e8612aa896a5fa17b0a4a0a04bbda5b82568499
tarungarg546 <tarungarg546@gmail.com>
2016-06-13 16:18:45 +0530

    [serviceWorker] bug fix

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 var cacheName="tarungarg546-github";
 self.addEventListener("install",function(event){
 	console.log("[ServiceWorker] installing...");
-	event.waitUntill(
+	event.waitUntil(
 						caches.open(cacheName)
 											.then(function(cache){
 												console.log('[ServiceWorker] Caching files');
